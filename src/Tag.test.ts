b77caeeff16641c60477e3a46a7031ea469066bc
kozak <kozak@192.168.1.10>
2024-06-12 18:57:00 +0300

    added vitest and eslint

--- src/Tag.test.ts
+++ src/Tag.test.ts
@@ -1,39 +1,38 @@
-import Tag from "./Tag";
+import { expect, test } from 'vitest';
+import Tag from './Tag';
 
-describe('Class Tag', () => {
-    it('should generate html tag (single tag, no params, no children)', () => {
-        const result = new Tag('input').toString();
-        const expected = '<input>';
-        expect(result).toBe(expected)
-    });
+test('should generate html tag (single tag, no params, no children)', () => {
+  const result = new Tag('input').toString();
+  const expected = '<input>';
+  expect(result).toBe(expected);
+});
 
-    it('should generate html tag (paired tag, no params, no children)', () => {
-        const result = new Tag('div').toString();
-        const expected = '<div></div>';
-        expect(result).toBe(expected)
-    });
+test('should generate html tag (paired tag, no params, no children)', () => {
+  const result = new Tag('div').toString();
+  const expected = '<div></div>';
+  expect(result).toBe(expected);
+});
 
-    it('should generate html tag (single tag, with params, no children)', () => {
-        const result = new Tag('img', { src: '/image.png', class: 'object-fit' }).toString();
-        const expected = '<img src="/image.png" class="object-fit">';
-        expect(result).toBe(expected)
-    });
+test('should generate html tag (single tag, with params, no children)', () => {
+  const result = new Tag('img', { src: '/image.png', class: 'object-fit' }).toString();
+  const expected = '<img src="/image.png" class="object-fit">';
+  expect(result).toBe(expected);
+});
 
-    it('should generate html tag (paired tag, with params, no children)', () => {
-        const result = new Tag('p', { class: 'px-2 py-5' }).toString();
-        const expected = '<p class="px-2 py-5"></p>';
-        expect(result).toBe(expected)
-    });
+test('should generate html tag (paired tag, with params, no children)', () => {
+  const result = new Tag('p', { class: 'px-2 py-5' }).toString();
+  const expected = '<p class="px-2 py-5"></p>';
+  expect(result).toBe(expected);
+});
 
-    it('should generate html tag (single tag, with params, with children)', () => {
-        const result = new Tag('img', { src: '/image.png', class: 'object-fit' }, 'ignored text').toString();
-        const expected = '<img src="/image.png" class="object-fit">';
-        expect(result).toBe(expected)
-    });
+test('should generate html tag (single tag, with params, with children)', () => {
+  const result = new Tag('img', { src: '/image.png', class: 'object-fit' }, 'ignored text').toString();
+  const expected = '<img src="/image.png" class="object-fit">';
+  expect(result).toBe(expected);
+});
 
-    it('should generate html tag (paired tag, with params, with children)', () => {
-        const result = new Tag('p', { class: 'px-2 py-5' }, 'Hello There!').toString();
-        const expected = '<p class="px-2 py-5">Hello There!</p>';
-        expect(result).toBe(expected)
-    });
+test('should generate html tag (paired tag, with params, with children)', () => {
+  const result = new Tag('p', { class: 'px-2 py-5' }, 'Hello There!').toString();
+  const expected = '<p class="px-2 py-5">Hello There!</p>';
+  expect(result).toBe(expected);
 });
--- src/Tag.ts
+++ src/Tag.ts
@@ -1,36 +1,36 @@
 const SINGLE_TAGS: string[] = ['input', 'img', 'br'];
 
 export default class Tag {
-    constructor(
-        private name: string,
-        private options?: Record<string, string>,
-        private children?: string
-    ) {}
+  constructor(
+    private name: string,
+    private options?: Record<string, string>,
+    private children?: string,
+  ) {}
 
-    toString(): string {
-        let result = `<${this.name}`;
+  toString(): string {
+    let result = `<${this.name}`;
 
-        if (this.options) {
-            const options = Object
-                .entries(this.options)
-                .map(([key, value]: [string, string]) => `${key}="${value}"`)
-                .join(' ');
-            result += ' ';
-            result += options;
-        }
-
-        result += '>';
-
-        if (SINGLE_TAGS.includes(this.name)) {
-            return result;
-        }
-
-        if (this.children) {
-            result += this.children;
-        }
-
-        result += `</${this.name}>`;
-
-        return result;
+    if (this.options) {
+      const options = Object
+        .entries(this.options)
+        .map(([key, value]: [string, string]) => `${key}="${value}"`)
+        .join(' ');
+      result += ' ';
+      result += options;
     }
+
+    result += '>';
+
+    if (SINGLE_TAGS.includes(this.name)) {
+      return result;
+    }
+
+    if (this.children) {
+      result += this.children;
+    }
+
+    result += `</${this.name}>`;
+
+    return result;
+  }
 }
