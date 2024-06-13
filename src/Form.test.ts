cf2cbe4ea0f91e2766a375758e13cac3c9997165
kozak <kozak@MacBook-Pro-kozak.local>
2024-06-13 12:04:49 +0300

    added a new class

--- src/Form.test.ts
+++ src/Form.test.ts
@@ -1,15 +1,14 @@
 import { expect, test } from 'vitest';
-// @ts-ignore
 import Form from './Form';
 
 test('should generate form (empty url)', () => {
-    const result = Form.formFor({}, {});
-    const expected = '<form method="post" action="#"></form>';
-    expect(result).toBe(expected);
+  const result = Form.formFor({}, {});
+  const expected = '<form method="post" action="#"></form>';
+  expect(result).toBe(expected);
 });
 
 test('should generate form (set url)', () => {
-    const result = Form.formFor({}, { url: '/submit' });
-    const expected = '<form method="post" action="/submit"></form>';
-    expect(result).toBe(expected);
+  const result = Form.formFor({}, { url: '/submit' });
+  const expected = '<form method="post" action="/submit"></form>';
+  expect(result).toBe(expected);
 });
