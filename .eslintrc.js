adda1b9b7d3c4f9426d8f2b93283f6037baf0f50
kozak <kozak@MacBook-Pro-kozak.local>
2024-06-14 17:13:27 +0300

    disable eslist for configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable fp/no-mutation */
 module.exports = {
   plugins: [
     '@typescript-eslint',
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable fp/no-mutation */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
